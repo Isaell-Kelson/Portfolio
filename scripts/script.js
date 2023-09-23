2331e02a0efee3bee7b67ac5e9fe4772e6477786
Isaell Kelson <isaellkelson@gmail.com>
2023-09-23 11:03:42 -0300

    aplication sass

---
+++ scripts/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-icon').addEventListener('click', function() {
+    document.querySelector('nav ul').classList.toggle('active');
+    document.querySelector('.menu-icon').classList.toggle('active');
+  });
+  
