07c91b5e5095c1a6169396e1e770042fd6a1cb99
Isaell Kelson <isaellkelson@gmail.com>
2023-09-30 10:19:31 -0300

    correction responsive

--- scripts/script.js
+++ scripts/script.js
@@ -1,5 +1,21 @@
+function closeMenu() {
+  document.querySelector('nav ul').classList.remove('active');
+  document.querySelector('.menu-icon').classList.remove('active');
+}
+
+
 document.querySelector('.menu-icon').addEventListener('click', function() {
-    document.querySelector('nav ul').classList.toggle('active');
-    document.querySelector('.menu-icon').classList.toggle('active');
-  });
+  document.querySelector('nav ul').classList.toggle('active');
+  document.querySelector('.menu-icon').classList.toggle('active');
+});
+
+
+document.addEventListener('click', function(event) {
+  const menu = document.querySelector('nav ul');
+  const menuIcon = document.querySelector('.menu-icon');
+
   
+  if (!menu.contains(event.target) && !menuIcon.contains(event.target)) {
+    closeMenu();
+  }
+});
